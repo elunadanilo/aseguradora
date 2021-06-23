c2da87a017bdd6795c02713a8e095a38fefd5e94
elunadanilo <eluna.danilo@gmail.com>
2021-06-22 22:58:47 -0600

    Pagina
    Se agrega maestro de pagina

--- Aseguradora/Aseguradora/Controllers/PaginaController.cs
+++ Aseguradora/Aseguradora/Controllers/PaginaController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Aseguradora.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,75 @@ namespace Aseguradora.Controllers
         // GET: Pagina
         public ActionResult Index()
         {
-            return View();
+            List<PaginaModel> listaPagina = new List<PaginaModel>();
+            using (var bd = new DbAseguradoraEntities())
+            {
+                listaPagina = (from pagina in bd.TblPagina
+                               where pagina.BHabilitado == 1
+                               select new PaginaModel
+                               {
+                                   IdPagina = pagina.IdPagina,
+                                   Mensaje = pagina.Mensaje,
+                                   Controlador = pagina.Controlador,
+                                   Accion = pagina.Accion
+                               }).ToList();
+            }
+                return View(listaPagina);
+        }
+
+        public ActionResult Filtrar(PaginaModel oPaginaModel)
+        {
+            string mensaje = oPaginaModel.Mensaje;
+            List<PaginaModel> listaPagina = new List<PaginaModel>();
+            using (var bd = new DbAseguradoraEntities())
+            {
+                if (mensaje == null)
+                {
+                    listaPagina = (from pagina in bd.TblPagina
+                                   where pagina.BHabilitado == 1
+                                   select new PaginaModel
+                                   {
+                                       IdPagina = pagina.IdPagina,
+                                       Mensaje = pagina.Mensaje,
+                                       Controlador = pagina.Controlador,
+                                       Accion = pagina.Accion
+                                   }).ToList();
+                }
+                else
+                {
+                    listaPagina = (from pagina in bd.TblPagina
+                                   where pagina.BHabilitado == 1
+                                   && pagina.Mensaje.Contains(mensaje)
+                                   select new PaginaModel
+                                   {
+                                       IdPagina = pagina.IdPagina,
+                                       Mensaje = pagina.Mensaje,
+                                       Controlador = pagina.Controlador,
+                                       Accion = pagina.Accion
+                                   }).ToList();
+                }
+            }
+
+            return PartialView("_TablaPagina",listaPagina);
+        }
+
+        public int Save(PaginaModel oPaginaModel, int Titulo)
+        {
+            int rpta = 0;
+            using (var bd = new DbAseguradoraEntities())
+            {
+                if (Titulo.Equals(1))
+                {
+                    TblPagina oPagina = new TblPagina();
+                    oPagina.Mensaje = oPaginaModel.Mensaje;
+                    oPagina.Accion = oPaginaModel.Accion;
+                    oPagina.Controlador = oPaginaModel.Controlador;
+                    oPagina.BHabilitado = 1;
+                    bd.TblPagina.Add(oPagina);
+                    rpta = bd.SaveChanges();
+                }
+            }
+            return rpta;
         }
     }
 }
