adaef9fdf06ee78ceb6b1b496ece83e419d2f60b
elunadanilo <eluna.danilo@gmail.com>
2021-06-22 23:29:58 -0600

    Controlador RolPagina
    Se agrega controlador rol pagina

---
+++ Aseguradora/Aseguradora/Controllers/RolPaginaController.cs
@@ -0,0 +1,34 @@
+﻿using Aseguradora.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aseguradora.Controllers
+{
+    public class RolPaginaController : Controller
+    {
+        // GET: RolPagina
+        public ActionResult Index()
+        {
+            List<RolPaginaModel> listaRol = new List<RolPaginaModel>();
+            using (var bd = new DbAseguradoraEntities())
+            {
+                listaRol = (from rolpagina in bd.TblRolPagina
+                            join rol in bd.TblRol
+                            on rolpagina.IdRol equals rol.IdRol
+                            join pagina in bd.TblPagina
+                            on rolpagina.IdPagina equals pagina.IdPagina
+                            select new RolPaginaModel
+                            {
+                                IdRolPagina=rolpagina.IdRolPagina,
+                                nombreRol=rol.Nombre,
+                                nombreMensaje=pagina.Mensaje
+                            }).ToList();
+
+            }
+                return View();
+        }
+    }
+}
---
+++ Aseguradora/Aseguradora/Models/RolPaginaModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Aseguradora.Models
+{
+    public class RolPaginaModel
+    {
+        [Display(Name = "Id Rol Pagina")]
+        public int IdRolPagina { get; set; }
+        [Required]
+        public int IdRol { get; set; }
+        [Required]
+        public int IdPagina { get; set; }
+        public int BHabilitado { get; set; }
+        //Propiedades adicionales
+        [Display(Name = "Nombre Rol")]
+        public string nombreRol { get; set; }
+        [Display(Name = "Nombre Mensaje")]
+        public string nombreMensaje { get; set; }
+    }
+}
